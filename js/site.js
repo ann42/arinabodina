e5f5832f980dbbc524c3c300f907b823904ffa2f
ann42 <kharchenko.ann@gmail.com>
2015-02-05 20:36:08 +0300

    in process

--- js/site.js
+++ js/site.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
     $(".button-collapse").sideNav();
 
-
+//$('ul.tabs').tabs();
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    })
 });
