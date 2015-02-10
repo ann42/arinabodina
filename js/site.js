0e977d128edd5cea9c55b8d2756dd3d585345ef6
ann42 <kharchenko.ann@gmail.com>
2015-02-10 18:18:19 +0300

    add google carts

--- js/site.js
+++ js/site.js
@@ -10,11 +10,38 @@ $(document).ready(function(){
       //$(cellsSelector).show();
     //})
 
-  function initialize() {
-    var mapOptions = {
-      zoom: 14,
-      scrollwheel: false
+
+    function initialize() {
+      var directionsService = new google.maps.DirectionsService();
+      var directionsDisplay = new google.maps.DirectionsRenderer();
+        
+      var mapOptions = {
+        center: new google.maps.LatLng(55.802898, 37.5110276),
+        zoom: 16,
+        scrollwheel: false
+      };
+
+      var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+        
+      directionsDisplay.setMap(map);
+        
+      var directionRequest = {
+          origin: new google.maps.LatLng(55.8042202,37.5161527),
+          destination: new google.maps.LatLng(55.8024409,37.5071098),
+          travelMode: google.maps.TravelMode.WALKING,
+          unitSystem: google.maps.UnitSystem.METRIC
+      };
+        
+      directionsService.route(directionRequest, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+        };
+      });
+
     }
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-  }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
 });
+
