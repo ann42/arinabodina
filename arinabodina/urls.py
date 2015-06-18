677c64a973752b9cfdb10267c557c167d0979c2b
ann42 <kharchenko.ann@gmail.com>
2015-06-18 12:11:59 +0300

    register in google, add new schedule, del unused animation in albums, add robotstxt

--- arinabodina/urls.py
+++ arinabodina/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from views import index, album, albums_list, videos_list
+from django.views.generic.simple import direct_to_template
 
 admin.autodiscover()
 
@@ -14,5 +15,6 @@ urlpatterns = patterns('',
     url(r'^$', index),
     url(r'^album/(\d+)/?$', album),
     url(r'^albums/?$', albums_list),
-    url(r'^videos/?$', videos_list)
+    url(r'^videos/?$', videos_list),
+    url(r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'})
 )
