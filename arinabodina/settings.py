83d90d290fa7ba922e0f28f3552c533b485ce102
anna <kharchenko.ann@gmail.com>
2015-06-21 00:12:01 +0300

    Upd Settings

--- arinabodina/settings.py
+++ arinabodina/settings.py
@@ -61,17 +61,13 @@ WSGI_APPLICATION = 'arinabodina.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost',
+        'HOST': 'grafanaliz.ru',
         'PORT': '',
-        'NAME': 'arinabodina',
-        'USER': 'root',
-        'PASSWORD': 'fish',
+        'NAME': 'byronles_arinabodina',
+        'USER': 'byronles_arbo',
+        'PASSWORD': '(ks3r#wb7x)*',
     }
 }
 
---
+++ arinabodina/settings_local.py
@@ -0,0 +1,10 @@
+from settings import *
+
+DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': '',
+        'NAME': 'arinabodina',
+        'USER': 'root',
+        'PASSWORD': 'fish',
+    }
